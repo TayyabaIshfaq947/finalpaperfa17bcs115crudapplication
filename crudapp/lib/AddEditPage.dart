edb7d91e3e4be274806889f0a0864c238d90be82
TayyabaIshfaq <tayyabaishfaq671@gmail.com>
2021-07-02 11:38:27 +0500

    added addedit page

---
+++ crudapp/lib/AddEditPage.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:crudapp/main.dart';
+
+import 'homepage.dart';
+
+class AddEditPage extends StatefulWidget {
+  final List list;
+  final int index;
+  AddEditPage({this.list, this.index});
+  @override
+  _AddEditPageState createState() => _AddEditPageState();
+}
+
+class _AddEditPageState extends State<AddEditPage> {
+  TextEditingController name = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController phone = TextEditingController();
+  TextEditingController address = TextEditingController();
+
+  bool editMode = false;
+
+  addUpdateData() {
+    if (editMode) {
+      var url = 'http://192.168.48.115/crudapp/edit.php';
+      http.post(Uri.parse(url), body: {
+        'id': widget.list[widget.index]['id'],
+        'name': name.text,
+        'email': email.text,
+        'Contact': phone.text,
+        'address': address.text,
+      });
+    } else {
+      var url = 'http://192.168.48.115/crudapp/add.php';
+      http.post(Uri.parse(url), body: {
+        'name': name.text,
+        'email': email.text,
+        'Contact': phone.text,
+        'address': address.text,
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.index != null) {
+      editMode = true;
+      name.text = widget.list[widget.index]['name'];
+      email.text = widget.list[widget.index]['email'];
+      phone.text = widget.list[widget.index]['phone'];
+      address.text = widget.list[widget.index]['address'];
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(editMode ? 'Update' : 'Add Data'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: name,
+              decoration: InputDecoration(
+                labelText: 'Name',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: email,
+              decoration: InputDecoration(
+                labelText: 'email',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: phone,
+              decoration: InputDecoration(
+                labelText: 'phone',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: address,
+              decoration: InputDecoration(
+                labelText: 'Address',
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: RaisedButton(
+              onPressed: () {
+                setState(() {
+                  addUpdateData();
+                });
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyHomePage(),
+                  ),
+                );
+                debugPrint('Clicked RaisedButton Button');
+              },
+              color: Colors.blue,
+              child: Text(
+                editMode ? 'Update' : 'Save',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
